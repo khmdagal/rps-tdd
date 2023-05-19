8333624c3b2c0f92aad1ccac245984d9a0b65818
khmdagal <khmdagal@gmail.com>
2023-05-19 22:37:25 +0100

    Second test - scissors vs. rock

--- index.test.js
+++ index.test.js
@@ -1,38 +1,43 @@
-
-
-function rps() {
-    return "left";
+function rps(left) {
+  return left === "rock" ? "left" : "right";
 }
 
 it("should work", () => {
   const left = 1;
   const right = 2;
 
-  const result = (left + right);
+  const result = left + right;
 
   expect(result).toBe(3);
 });
 
 it("should work", () => {
-    const left = 2;
-    const right = 2;
+  const left = 2;
+  const right = 2;
 
-    const result = left + right;
+  const result = left + right;
 
-    expect(result).toBe(5);
+  expect(result).toBe(4);
 });
-
 
 //using describe function, use for group of tests, and referred as "suite"
 
 describe("rock, pager, scissors", () => {
-    it("should say left wins for rock vs. scissors", () => {
-        
-        const left = "rock";
-        const right = "scissors";
+  it("should say left wins for rock vs. scissors", () => {
+    const left = "rock";
+    const right = "scissors";
 
-        const outcome = rps(left, right);
+    const outcome = rps(left, right);
 
-        expect(outcome).toBe("left");
-    });
+    expect(outcome).toBe("left");
+  });
+
+  it("should say right wins for scissors vs. rock", () => {
+    const left = "scissors";
+    const right = "rock";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("right");
+  });
 });
